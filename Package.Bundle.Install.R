7ad68482ce1f7d9d563926c90b6891e0b3b545c1
David Doret <david.doret@me.com>
2013-08-05 00:18:38 +0200

    Setup
    My install script with type="source" functions properly and the .tar.gz
    file is ok.

--- Package.Bundle.Install.R
+++ Package.Bundle.Install.R
@@ -6,15 +6,13 @@ if(!require(devtools)){
   library(devtools)
 }
 if(require(devtools)){
- devtools::install_github() 
+# devtools::install_github() 
 
- #for compile binaries install :
-#   install.packages('xxx', repo='http://repo_adress')
-# for source install :
-#   install.packages('xxx', repo='http://repo_adress', type='source')
- 
-}
-else
-{
+  package.name <- "miscr1"
+  package.url <- "https://github.com/daviddoret/miscr1/raw/master/Package/miscr1.tar.gz"
+  
+  # Curiously, I encountered various errors with the install_github function.
+  install_url(url = package.url)
+}else{
   warning("Bundled R packages are available for development purposes. Hence, they require the \"devtools\" package. Please install the \"devtools\" package with the following command: \"install.packages('devtools')\".")
 }
